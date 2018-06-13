e20f386f4b4601a95e852d315516d811f92dc293
Jeferson Alves <jefersonalvesf2009@gmail.com>
2018-06-13 12:27:22 -0300

    Add api support to receive many values

--- api/views.py
+++ api/views.py
@@ -19,10 +19,10 @@ class BiodigesterList(APIView):
         return Response(serializer.data)
 
     def post(self, request, format=None):
-        serializer = BiodigesterSerializer(data=request.data)
+        serializer = BiodigesterSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class BiodigesterDetail(APIView):
